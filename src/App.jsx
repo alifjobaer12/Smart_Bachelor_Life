ae5549238f8d6ab36c6ae6b33a1d74b28807c475
alifjobaer12 <alifjobaer12@gmail.com>
2026-01-29 22:21:23 +0600

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import LandingPage from './pages/LandingPage';
+import Sample from './component/Sample.jsx';
 
 const App = () => {
   return (
     <div>
       <LandingPage></LandingPage>
+      <Sample/>
     </div>
   );
 };

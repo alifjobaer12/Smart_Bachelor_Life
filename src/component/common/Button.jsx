1ef4cf4ac760ad87ff1b9edc5a5f612051b5020e
alifjobaer12 <alifjobaer12@gmail.com>
2026-01-31 12:43:52 +0600

    fix theme

--- src/component/common/Button.jsx
+++ src/component/common/Button.jsx
@@ -4,11 +4,7 @@ import { motion } from "framer-motion";
 
 const Button = ({ className = "", size = "default", children, ...props }) => {
 	const baseClasses =
-<<<<<<< HEAD
-		"w-[144px] h-[50px] relative overflow-hidden rounded-lg font-medium bg-primary text-primary-foreground hover:bg-highlight hover:text-secondary-foreground   hover:border-primary hover:border  shadow-lg shadow-highlight/25";
-=======
-		"w-[144px] h-[50px] relative overflow-hidden rounded-[8px] font-medium bg-primary text-primary-foreground hover:bg-highlight hover:text-primary dark:hover:text-primary-foreground hover:border-primary shadow-lg shadow-highlight/25";
->>>>>>> 73e530e (make navbar)
+		"w-[144px] h-[50px] relative overflow-hidden rounded-lg font-medium bg-primary text-primary-foreground hover:bg-highlight hover:text-secondary-foreground   hover:border-primary hover:border-1  shadow-lg shadow-highlight/25";
 
 	const sizeClasses = {
 		sm: "px-4 py-2 text-sm",
